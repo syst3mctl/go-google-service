02e5e6944a700c16e32d2b71e3a2bc19016a25f7
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2024-03-26 14:33:33 +0400

    update: improve error handling, write comments

--- oauth2.go
+++ oauth2.go
@@ -10,14 +10,27 @@ import (
 	"os"
 )
 
-// Retrieves a token, saves the token, then returns the generated client.
-func GetClient(config *oauth2.Config) *http.Client {
-	tokFile := "token.json"
-	tok, err := tokenFromFile(tokFile)
+// GetClient func retrieves a token, saves the token, then returns the generated client.
+//
+// Parameters:
+//
+//	-Pointer of oauth2.Config
+//	-File name of tokenFile should be string
+//
+// Returns:
+//
+//	-Pointer of http.Client
+func GetClient(config *oauth2.Config, tokenFile string) *http.Client {
+	// retrieve token from given file
+	tok, err := tokenFromFile(tokenFile)
 	if err != nil {
+		// get token from web
 		tok = getTokenFromWeb(config)
-		saveToken(tokFile, tok)
+		// save token in file
+		saveToken(tokenFile, tok)
 	}
+
+	// return generated client
 	return config.Client(context.Background(), tok)
 }
 
@@ -32,10 +45,12 @@ func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
 		log.Fatalf("Unable to read authorization code: %v", err)
 	}
 
-	tok, err := config.Exchange(oauth2.NoContext, authCode)
+	// while oauth2.NoContext is depreciated, replaced with context.TODO()
+	tok, err := config.Exchange(context.TODO(), authCode)
 	if err != nil {
 		log.Fatalf("Unable to retrieve token from web: %v", err)
 	}
+
 	return tok
 }
 
@@ -46,18 +61,27 @@ func tokenFromFile(file string) (*oauth2.Token, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	tok := &oauth2.Token{}
 	err = json.NewDecoder(f).Decode(tok)
+
 	return tok, err
 }
 
 // Saves a token to a file path.
 func saveToken(path string, token *oauth2.Token) {
 	fmt.Printf("Saving credential file to: %s\n", path)
+	//  read and write the file or directory
 	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+
 	defer f.Close()
+
 	if err != nil {
 		log.Fatalf("Unable to cache OAuth token: %v", err)
 	}
-	json.NewEncoder(f).Encode(token)
+
+	err = json.NewEncoder(f).Encode(token)
+	if err != nil {
+		log.Fatalf("Unable to encode token, got error: %v", err)
+	}
 }
